8021415fb3da62254178eba5d4f56efe739845fd
Kris Nuttycombe <kris.nuttycombe@gmail.com>
2011-04-03 22:57:59 -0600

    Switched to using vectors for doc & metadata components to support fast appends.

--- src/main/scala/com/nommit/pickle/Doc.scala
+++ src/main/scala/com/nommit/pickle/Doc.scala
@@ -1,13 +1,25 @@
 package com.nommit.pickle
 
+import collection.{IndexedSeqLike, TraversableLike}
+import collection.immutable.{IndexedSeq, Vector}
+import collection.generic.{SeqFactory, CanBuildFrom, HasNewBuilder}
+
 object Doc {
-  val Empty = Doc(Nil)
-  def text(s: String) = Doc(List(Text(s)))
-  def tagged(t: Semantics, d: Doc) = Doc(List(Tagged(t, d)))
-  def apply(s: Section*): Doc = Doc(s.toList)
+  val Empty = new Doc(Vector.empty)
+  def text(s: String) = new Doc(Vector(Text(s)))
+  def tagged(t: Semantics, d: Doc) = new Doc(Vector(Tagged(t, d)))
+  def apply(s: Section*): Doc = new Doc(Vector(s: _*))
+  def unapplySeq(doc: Doc) = Some(doc.sections)
+  def newBuilder = Vector.newBuilder[Section].mapResult(new Doc(_))
 }
 
-case class Doc private[pickle] (sections: List[Section])
+class Doc private[pickle] (private[pickle] val sections: Vector[Section]) extends IndexedSeq[Section] with IndexedSeqLike[Section, Doc] {
+  override protected[this] def newBuilder = Doc.newBuilder
+
+  override def apply(idx: Int) = sections(idx)
+
+  override def length = sections.length
+}
 
 
 
--- src/main/scala/com/nommit/pickle/PickleParser.scala
+++ src/main/scala/com/nommit/pickle/PickleParser.scala
@@ -10,7 +10,7 @@ object PickleParser extends RegexParsers with PackratParsers {
   // or would confuse the parser can be used as an identifier. Bring on your Unicode!
   val Ident = """([^@\[\]\s\p{Cntrl}]+)""".r
 
-  lazy val doc: PackratParser[Doc] = rep(tagged | text).map(Doc(_))
+  lazy val doc: PackratParser[Doc] = rep(tagged | text).map(Doc(_: _*))
 
   def text: Parser[Text] = """([^@\\]|\\@)+""".r ^^ (s => Text(s))
 
@@ -22,11 +22,11 @@ object PickleParser extends RegexParsers with PackratParsers {
 
   lazy val longForm: PackratParser[Tagged]  = ("@" ~> tagbody ~ doc ~ longFormClose) flatMap {
     // translate long form to short form if the data is in ident form
-    case (Doc(List(Text(Ident(oid)))), metadata) ~ doc ~ None => success(Tagged(Tag(oid, metadata), doc))
+    case (Doc(Text(Ident(oid))), metadata) ~ doc ~ None => success(Tagged(Tag(oid, metadata), doc))
     // short close always succeeds
     case (metadoc, metadata) ~ doc ~ None => success(Tagged(MetaTag(metadoc, metadata), doc))
     // long close succeeds if the opening tag body is in ident form and opening id matches closing id
-    case (Doc(List(Text(oid))), metadata) ~ doc ~ Some(cid) if oid.trim == cid.trim => success(Tagged(Tag(oid, metadata), doc))
+    case (Doc(Text(oid)), metadata) ~ doc ~ Some(cid) if oid.trim == cid.trim => success(Tagged(Tag(oid, metadata), doc))
     // long close cannot be used for doc form
     case body ~ doc ~ c => failure("Unable to match metatag closing tag for " + body + " at " + c)
   }
@@ -34,13 +34,13 @@ object PickleParser extends RegexParsers with PackratParsers {
   lazy val longFormClose: PackratParser[Option[String]] = ("@/" ^^ (_ => None) | ("@[/" ~> Ident <~ "]").map(Some(_)))
 
   lazy val tagbody: PackratParser[(Doc, Metadata)] = "[" ~> ndoc ~ opt("|" ~> rep1sep(shortForm, "\\w+".r)) <~ "]" ^^ {
-    case d ~ m => (d, Metadata(m.toList.flatten))
+    case d ~ m => (d, Metadata(m.flatten.toSeq: _*))
   }
 
   def ntext: Parser[Text] = """([^|@\\\]\[]|\\@|\\\||\\ )+""".r ^^ (s => Text(s))
 
   lazy val ndoc: PackratParser[Doc] = rep(tagged | ntext).map(
-    l => Doc(
+    l =>  Doc(
       l.foldRight(List.empty[Section]) {
         case (Text(s), Nil) =>
           // strip trailing unescaped spaces from the last element
@@ -49,7 +49,7 @@ object PickleParser extends RegexParsers with PackratParsers {
           if (ss.isEmpty) Nil else Text(ss) :: Nil
 
         case (elem, rest) => elem :: rest
-      }
+      } : _*
     )
   )
 }
